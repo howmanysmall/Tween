d5b0bfe87eee0e87a83e25fbcff2663a717205d7
howmanysmall <9477871+1@iechs.org>
2017-12-06 12:00:48 -0700

    Update Bezier.lua
    Edited formatting.

--- Bezier.lua
+++ Bezier.lua
@@ -9,11 +9,11 @@ return {
 		-- @resources http://greweb.me/bezier-easing-editor/example/, http://cubic-bezier.com/
 		
 		if n and r and a and w then
-			local l, z = 3*a, 3*n
-			w, r = 3*w, 3*r
-			a, n = 6*(a - 2*n), 1 - w + r
-			local m, e, q = 1 - l + z, l - 2*z, w - 2*r
-			local o = 3*m
+			local l, z = 3 * a, 3 * n
+			w, r = 3 * w, 3 * r
+			a, n = 6 * (a - 2 * n), 1 - w + r
+			local m, e, q = 1 - l + z, l - 2 * z, w - 2 * r
+			local o = 3 * m
 	
 			return function(t, b, c, d)
 				-- @param number t [0, 1]
@@ -22,13 +22,13 @@ return {
 				t = (c or 1) * t / (d or 1) + (b or 0)
 				local f = t
 				for _ = 1, 4 do
-					local y = f*(a + o*f) + z
+					local y = f * (a + o * f) + z
 					if y == 0 then
 						break
 					end
-					f = f - (((m*f + e)*f + z)*f - t) / y
+					f = f - (((m * f + e) * f + z) * f - t) / y
 				end
-				return ((n*f + q)*f + r)*f
+				return ((n * f + q) * f + r) * f
 			end
 		else
 			error("[Bezier] - Need 4 numbers to construct a Bezier curve")
